b50d9564d1702e25af8d3043dff32bfe2ca9a2f9
andrey.chupakhin <andrey.chupakhin@edgecenter.ru>
2023-09-20 02:54:07 +0500

    refactor(main): remove comments

--- sky_pro/users/urls.py
+++ sky_pro/users/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URL для входа (авторизации)
     path('login/', views.login_view, name='login'),
-
-    # URL для выхода (логаута)
     path('logout/', views.logout_view, name='logout'),
-
-    # URL для регистрации (если используется предыдущий код)
     path('register/', views.register, name='register'),
 ]
