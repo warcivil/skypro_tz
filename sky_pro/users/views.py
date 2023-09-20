0cd67fb81a634259fbd7d2cdf90befbcf557c0f0
andrey.chupakhin <andrey.chupakhin@edgecenter.ru>
2023-09-20 17:38:24 +0500

    feat(add_form):refacor user views

--- sky_pro/users/views.py
+++ sky_pro/users/views.py
@@ -1,34 +1,19 @@
-from django.contrib.auth import logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+
+class RegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = '/home/products/'
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('product_list')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    authentication_form = AuthenticationForm
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('product_list')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+class CustomLogoutView(LogoutView):
+    next_page = '/users/login/'
